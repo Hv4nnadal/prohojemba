34baa62ecc014e95cb5f2a15cc9a67b96654ea62
umaruch <60070943+umaruch@users.noreply.github.com>
2022-01-05 12:38:31 +0500

    get user by id function

--- back/routes/users.py
+++ back/routes/users.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
 
 from back.crud import users
 from back.services import auth_service
@@ -17,13 +18,19 @@ async def get_current_user(
 async def get_user_by_id(
         user_id: int,
         current_user_id: int = Depends(auth_service.check_access_token),
-
 ):
-    pass
+    user = await users.get_by_id(user_id)
+    if user:
+        return user
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found."
+    )
 
 
 @users_router.put("")
 async def update_user(
-
+    current_user_id: int = Depends(auth_service.check_access_token),
 ):
     pass
